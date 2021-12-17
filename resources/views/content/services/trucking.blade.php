c2b6d20e767098a24024aa374687cf739114c891
Ssmurf8571 <62999778+Ssmurf8571@users.noreply.github.com>
2021-12-17 12:27:43 +0300

    1.1.4

--- resources/views/content/services/trucking.blade.php
+++ resources/views/content/services/trucking.blade.php
@@ -22,12 +22,17 @@
         </div>
     </section>
 
-    <div class="name-title"><h1 class="text-gray-700 text-capitalize fw-bolder text-center">trucking</h1><div class="title-border-bottom my-4 mx-auto"></div></div>
+    <div class="name-title"><h1 id="raplace" class="text-gray-700 text-capitalize fw-bolder text-center">trucking</h1><div class="title-border-bottom my-4 mx-auto"></div></div>
         
     @endsection
 
     @extends('inc.header')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/e21e861c03.js" crossorigin="anonymous"></script>
+    
+    <script>
+        $("#raplace").text($('#raplace').text().replace(/-/g," "));
+    </script>
 </body>
 </html>
